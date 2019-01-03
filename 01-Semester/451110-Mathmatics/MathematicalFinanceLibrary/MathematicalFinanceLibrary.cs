1b011df2b88eeecb01bef51bd65cce721b77ebcd
SundownEffect <tse.sunset@gmail.com>
2019-01-03 16:53:27 +0100

    451110-Mathmatics

---
+++ 01-Semester/451110-Mathmatics/MathematicalFinanceLibrary/MathematicalFinanceLibrary.cs
@@ -0,0 +1,26 @@
+﻿/* * * * * * *
+ * Title:   MathematicalFinanceLibrary
+ * Class:   MathematicalFinanceLibrary.cs
+ * Author:  Bräunlich, Christian
+ * Date:    03.01.2019
+ * 
+*/
+
+#region Library of Alexandria
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace MathematicalFinanceLibrary
+{
+    public static class MathematicalFinanceLibrary
+    {
+
+    }
+}
---
+++ 01-Semester/451110-Mathmatics/MathematicalFinanceLibrary/Program.cs
@@ -0,0 +1,29 @@
+﻿/* * * * * * *
+ * Title:   MathematicalFinanceLibrary
+ * Class:   Program.cs
+ * Author:  Bräunlich, Christian
+ * Date:    03.01.2019
+ * 
+*/
+
+#region Library of Alexandria
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace MathematicalFinanceLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
