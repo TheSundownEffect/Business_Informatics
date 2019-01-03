4585b1b055ac2f7e3c157ce1c0d244f39fd5e9cd
SundownEffect <tse.sunset@gmail.com>
2019-01-03 16:53:18 +0100

    451335-Corporate_Finance

---
+++ 03-Semester/451335-Corporate_Finance/Corporate_Finance/FinancialLibrary.cs
@@ -0,0 +1,26 @@
+﻿/* * * * * * *
+ * Title:   Corporate_Finance
+ * Class:   FinancialLibrary.cs
+ * Author:  Bräunlich, Christian
+ * Date:    03.01.2019
+ * 
+*/
+
+#region Library of Alexandria
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace Corporate_Finance
+{
+    public static class FinancialLibrary
+    {
+
+    }
+}
